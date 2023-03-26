67f4141df68d65040c97fadacf26655407ea6a84
fxhxyz4 <fxhxyz@proton.me>
2023-03-26 16:27:16 +0300

    add partials

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 
 const express = require('express');
 const colors = require('colors');
+const path = require('path');
 
 const PORT = process.env.PORT || 3939;
 const app = express();
 
+app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs').__express);
+
 app.get('/', (req, res) => {
-	res.send('./src/index.ejs');
+	res.render('index', { title: 'Home' });
 	res.status(200);
 });
 
