fd1b6ca78ad30f37f45b4dbc22212bce261c5850
fxhxyz4 <fxhxyz@proton.me>
2023-03-26 15:25:11 +0300

    add 404 page

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ require('dotenv').config();
 
 const express = require('express');
 const colors = require('colors');
-const routes = require('./src/routes/route');
 
 const PORT = process.env.PORT || 3939;
 const app = express();
 
 app.listen(PORT, () => {
+	if (!process.env.PORT) console.error(`error`.red);
 	console.debug(`SERVER STARTED ON PORT: ${PORT}`.toLowerCase().rainbow);
 });
 
-app.get(routes);
+app.get('*', (req, res) => {
+	res.send('./src/index.ejs');
+	res.status(200);
+});
--- src/routes/route.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-
-router.get('/', (req, res) => {
-	res.send('../index.ejs');
-	res.sendStatus(200);
-});
-
-module.exports = router;
