908e48cb1b406757d0c9f1aea7aceced2f36edb7
fxhxyz4 <fxhxyz@proton.me>
2023-03-26 15:36:55 +0300

    add recomend ext

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const colors = require('colors');
 const PORT = process.env.PORT || 3939;
 const app = express();
 
+app.get('/', (req, res) => {
+	res.send('./src/index.ejs');
+	res.status(200);
+});
+
 app.listen(PORT, () => {
 	if (!process.env.PORT) console.error(`error`.red);
 	console.debug(`SERVER STARTED ON PORT: ${PORT}`.toLowerCase().rainbow);
-});
-
-app.get('*', (req, res) => {
-	res.send('./src/index.ejs');
-	res.status(200);
 });
