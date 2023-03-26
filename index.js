c0cd1ea8f87c8ccf9faaefca67f2dd5426798fc4
fxhxyz4 <fxhxyz@proton.me>
2023-03-26 16:45:09 +0300

    add files to partials

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const colors = require('colors');
 const path = require('path');
+const ejs = require('ejs');
 
 const PORT = process.env.PORT || 3939;
 const app = express();
 
 app.set('view engine', 'ejs');
-app.engine('ejs', require('ejs').__express);
+app.engine('ejs', ejs.__express);
 
 app.get('/', (req, res) => {
 	res.render('index', { title: 'Home' });
