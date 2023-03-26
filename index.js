408378577b95fba91a14f0c13656db5fe2fce595
fxhxyz4 <fxhxyz@proton.me>
2023-03-26 13:40:38 +0300

    add nodemon config

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const colors = require('colors');
 
 const PORT = process.env.PORT || 3939;
 const app = express();
+
+app.listen(PORT, () => {
+	console.debug(`runned on port: ${PORT}`.toLowerCase().rainbow);
+});
+
+app.get('/', (req, res) => {
+	res.send('./src/index.ejs');
+	res.sendStatus(200);
+});
