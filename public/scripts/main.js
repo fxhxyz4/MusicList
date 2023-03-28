29057c1a538c760ae6f495517135bc0ce7a3d445
fxhxyz4 <fxhxyz@proton.me>
2023-03-28 17:15:59 +0300

    commit

--- public/scripts/main.js
+++
@@ -1,7 +0,0 @@
-import refs from './modules/refs.js';
-
-refs.formEl.addEventListener('submit', e => {
-	e.preventDefault();
-
-	refs.formEl.reset();
-});
--- public/scripts/modules/refs.js
+++
@@ -1,5 +0,0 @@
-const refs = {
-	formEl: document.querySelector('.nav-form__post'),
-};
-
-export default refs;
