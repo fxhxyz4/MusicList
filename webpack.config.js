ff45ae7c93ad54a73ad3fa6d359786d16d3c0df3
fxhxyz4 <fxhxyz@proton.me>
2023-04-15 15:05:25 +0300

    add webpack.EnvironmentPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const public_path = `./public/scripts/`;
 const __dirname = path.dirname(public_path);
@@ -14,9 +17,10 @@ export default {
 		topLevelAwait: true,
 	},
 	plugins: [
-		new webpack.DefinePlugin({
+		new webpack.EnvironmentPlugin({
 			'process.env.SPOTIFY_ID': JSON.stringify(process.env.SPOTIFY_ID),
 			'process.env.SPOTIFY_SECRET': JSON.stringify(process.env.SPOTIFY_SECRET),
 		}),
+		new webpack.EnvironmentPlugin(['SPOTIFY_ID', 'SPOTIFY_SECRET']),
 	],
 };
