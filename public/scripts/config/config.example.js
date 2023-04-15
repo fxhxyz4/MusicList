a15d243650551fa977afef4bdd1db3cfc597debb
fxhxyz4 <fxhxyz@proton.me>
2023-04-15 14:38:05 +0300

    edit js files

--- public/scripts/config/config.example.js
+++
@@ -1,4 +0,0 @@
-export default config = {
-	client_id: 'SPOTIFY_CLIENT_ID',
-	client_secret: 'SPOTIFY_CLIENT_SECRET',
-};
--- public/scripts/modules/spotify.js
+++ public/scripts/modules/spotify.js
@@ -1,28 +1,28 @@
 import refs from './refs.js';
-import config from '../config/config.js';
 import license from '../../../data.json';
 
-const client_id = config.client_id;
-const client_secret = config.client_secret;
-const token_endpoint = 'https://accounts.spotify.com/api/token';
+const SPOTIFY_ID = process.env.SPOTIFY_ID;
+const SPOTIFY_SECRET = process.env.SPOTIFY_SECRET;
+
+const token_uri = 'https://accounts.spotify.com/api/token';
 const licenseArray = license.data;
 
-const authString = `${client_id}:${client_secret}`;
-const encodedAuthString = btoa(authString);
-
-const response = await fetch(token_endpoint, {
-	method: 'POST',
-	headers: {
-		Authorization: `Basic ${encodedAuthString}`,
-		'Content-Type': 'application/x-www-form-urlencoded',
-	},
-	body: 'grant_type=client_credentials',
-});
-
-const data = await response.json();
-const token = data.access_token;
+const authParams = `${SPOTIFY_ID}:${SPOTIFY_SECRET}`;
+const encodedAuthString = btoa(authParams);
 
 export async function searchSpotify(query) {
+	const res = await fetch(token_uri, {
+		method: 'POST',
+		headers: {
+			Authorization: `Basic ${encodedAuthString}`,
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		body: 'grant_type=client_credentials',
+	});
+
+	const d = await res.json();
+	const token = d.access_token;
+
 	try {
 		const r = await fetch(`https://api.spotify.com/v1/search?type=track&q=${query}`, {
 			headers: {
