a1f57bf33c64b3a0b97e7a32a533df2a973dc1fe
fxhxyz <fxhxyz@proton.me>
2023-07-09 19:55:05 +0300

    edit webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,4 @@
 import path from 'path';
-import webpack from 'webpack';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const public_path = `./public/scripts/`;
 const __dirname = path.dirname(public_path);
@@ -16,11 +12,4 @@ export default {
 	experiments: {
 		topLevelAwait: true,
 	},
-	plugins: [
-		new webpack.EnvironmentPlugin({
-			'process.env.SPOTIFY_ID': JSON.stringify(process.env.SPOTIFY_ID),
-			'process.env.SPOTIFY_SECRET': JSON.stringify(process.env.SPOTIFY_SECRET),
-		}),
-		new webpack.EnvironmentPlugin(['SPOTIFY_ID', 'SPOTIFY_SECRET']),
-	],
 };
