3ae3019659c413f33f4b9244b37f2c5d85f7b9eb
fxhxyz <fxhxyz@proton.me>
2023-08-05 14:28:13 +0300

    added spin ref

--- public/scripts/modules/refs.js
+++ public/scripts/modules/refs.js
@@ -3,6 +3,7 @@ const refs = {
 	inputEl: document.querySelector('.nav-form__input'),
 	loginBtn: document.querySelector('.nav-auth__link'),
 	listEl: document.querySelector('.main__list'),
+  spinEl: document.querySelector('.spin'),
 };
 
 export { refs };
