ccc5ca4ca81d37cd01065d7f80a3c272243a307d
fxhxyz <fxhxyz@proton.me>
2023-08-06 22:32:48 +0300

    added fetch func

--- public/scripts/modules/handle.js
+++ public/scripts/modules/handle.js
@@ -1,23 +1,48 @@
 import { refs } from './refs.js';
 
-async function handleCallback() {
+function handleCallback() {
   const popup = window.open('/auth/twitch');
 
+  const interval = setInterval(() => {
+    if (popup.closed) {
+      clearInterval(interval);
+    } else {
+      try {
+        if (popup.location.href.includes('/auth/twitch/callback')) {
+          const url = new URL(popup.location.href);
+          const authCode = url.searchParams.get('code');
 
+          popup.close();
+          fetchData(authCode);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }, 1000);
+}
+
+async function fetchData(authCode) {
   try {
-    const response = await fetch('/auth/twitch/callback', {
+    const res = await fetch(`/auth/twitch/callback?code=${authCode}`, {
       method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+      },
     });
 
-    if (!response.ok) {
-      console.error('server err');
-    } else {
-      const loginData = await response.json();
-      console.log(loginData);
+    const { login, code } = await res.json();
+
+    if (login === true) {
+      localStorage.setItem('_code', code);
+      localStorage.setItem('_login', login);
+
+      refs.loginBtn.textContent = `Logout`;
     }
   } catch (e) {
-    console.error(`fetch err: ${e}`);
+    console.error(e);
   }
 }
+
 
 export { handleCallback };
