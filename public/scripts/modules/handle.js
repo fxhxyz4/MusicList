a17f31ed6182b4ec31508bbfcedce6439b4f6e25
fxhxyz <fxhxyz@proton.me>
2023-08-06 17:02:04 +0300

    -

--- public/scripts/modules/handle.js
+++ public/scripts/modules/handle.js
@@ -4,7 +4,24 @@ async function handleCallback() {
   const popup = window.open('/auth/twitch');
 
   const message = await new Promise((resolve) => {
-    window.addEventListener('message', resolve);
+    const handleResponse = (e) => {
+      if (e.origin === window.location.origin) {
+        const data = e.data;
+        console.log(data);
+
+        if (data.login === true) {
+          refs.loginBtn.textContent = `Logout`;
+        } else {
+          console.error(`error`);
+        }
+
+        popup.close();
+        window.removeEventListener('message', handleResponse);
+        resolve(e);
+      }
+    };
+
+    window.addEventListener('message', handleResponse);
   });
 
   const { data } = message;
@@ -16,7 +33,6 @@ async function handleCallback() {
   }
 
   popup.close();
-  window.removeEventListener('message', handleResponse);
 }
 
 export { handleCallback };
