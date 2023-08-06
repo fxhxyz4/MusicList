734c0284983ac9a217f6a37a50c7547b1b5e04a5
fxhxyz <fxhxyz@proton.me>
2023-08-06 17:19:51 +0300

    -

--- public/scripts/modules/handle.js
+++ public/scripts/modules/handle.js
@@ -3,6 +3,21 @@ import { refs } from './refs.js';
 async function handleCallback() {
   const popup = window.open('/auth/twitch');
 
+
+  try {
+    const response = await fetch('/auth/twitch/callback', {
+      method: 'GET',
+    });
+
+    if (!response.ok) {
+      console.error('server err');
+    } else {
+      const loginData = await response.json();
+      console.log(loginData);
+    }
+  } catch (e) {
+    console.error(`fetch err: ${e}`);
+  }
 }
 
 export { handleCallback };
