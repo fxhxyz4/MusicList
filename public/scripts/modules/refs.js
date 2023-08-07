51118e302c21fed96e86621be0b05fd6632d0aec
fxhxyz <fxhxyz@proton.me>
2023-08-07 09:52:35 +0300

    added btntxt ref

--- public/scripts/modules/refs.js
+++ public/scripts/modules/refs.js
@@ -1,4 +1,5 @@
 const refs = {
+	loginTxt: document.querySelector('.nav-auth__text'),
 	formEl: document.querySelector('.nav-form__post'),
 	inputEl: document.querySelector('.nav-form__input'),
 	loginBtn: document.querySelector('.nav-auth__link'),
