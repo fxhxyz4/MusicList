a35ce5ef78b5055363c384acdc183ac3ef09d561
fxhxyz <fxhxyz@proton.me>
2023-08-07 09:52:56 +0300

    added handleText func

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -26,3 +26,25 @@ refs.formEl.addEventListener('submit', e => {
 });
 
 refs.loginBtn.addEventListener('click', handleCallback);
+
+function handleText() {
+  const bool = localStorage.getItem('_login');
+  let btnTxt = '';
+
+  if (window.innerWidth <= 100) {
+    btnTxt = '';
+  } else if (window.innerWidth <= 800) {
+    btnTxt = '';
+  } else if (bool === 'true') {
+    btnTxt = 'Logged';
+  } else if (window.innerWidth <= 1400) {
+    btnTxt = 'Login';
+  } else {
+    btnTxt = 'Login with Twitch';
+  }
+
+  refs.loginTxt.textContent = btnTxt;
+}
+
+window.addEventListener('load', handleText);
+window.addEventListener('resize', handleText);
