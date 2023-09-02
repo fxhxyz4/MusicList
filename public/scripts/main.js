3c273ca666e02bec1c27ad9a290e9a4d136b889f
fxhxyz4 <fxhxyz@proton.me>
2023-09-02 20:32:42 +0300

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,10 @@
-console.clear();
-
 import { refs } from './modules/refs.js';
 import { searchSpotify } from './modules/fetch.js';
 import { handleCallback } from './modules/handle.js';
 
 refs.formEl.addEventListener('submit', e => {
 	e.preventDefault();
-
 	const value = refs.inputEl.value.trim();
-
-	localStorage.setItem('_input', value);
-	const inputStorage = localStorage.getItem('_input');
 
 	if (!value) {
 		refs.inputEl.placeholder = `[error]`;
