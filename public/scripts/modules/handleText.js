b95172beb3169a2de10e35f5599d5c21a16fd4a9
fxhxyz <fxhxyz@proton.me>
2023-09-14 21:15:26 +0300

    added handleText file

---
+++ public/scripts/modules/handleText.js
@@ -0,0 +1,20 @@
+function handleText() {
+  const bool = localStorage.getItem('_login');
+  let btnTxt = '';
+
+  if (window.innerWidth <= 100) {
+    btnTxt = '';
+  } else if (window.innerWidth <= 800) {
+    btnTxt = '';
+  } else if (bool === 'true') {
+    btnTxt = 'Logged';
+  } else if (window.innerWidth <= 1400) {
+    btnTxt = 'Login';
+  } else {
+    btnTxt = 'Login with Twitch';
+  }
+
+  refs.loginTxt.textContent = btnTxt;
+}
+
+export { handleText };
